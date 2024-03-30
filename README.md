4a13ad279363cdd467c95ce1109b172ef75cc1d8
shohanR <shohanur.sarker2018@northsouth.edu>
2024-03-30 11:56:42 +0600

    commented on main.go and updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cli-calculator
-This project implements the functions of a basic calculator. You can consume it in your terminal. 
+
+This project implements basic funtionalities of a calculator. It can be run in any terminal. 
+
+This calculator has four operations. They are :
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+
+## How to run
+
+***1. Clone the repository:***
+
+    git clone https://github.com/shohanR/cli-calculator.git
+    cd your-repository
+
+***2. Run the cli-calculator:***
+
+    go run main.go
+
+***3. Interact with Menu and Repeat or, Exit:***
+- Follow the on-screen instructions and make choices
+
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// declaring variables
 	var operationChoice int
 	var number1, number2, result float64
 	result = 0
@@ -10,11 +11,14 @@ func main() {
 
 	fmt.Println("\n\nWelcome to the GoLang CLI Calculator. Following are the options: ")
 
+	// infinite loop for continuous calculating
 	for {
 		fmt.Print("\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n\n")
 		fmt.Print("Enter operation: ")
 		fmt.Scan(&operationChoice)
 
+		// flag is used to check whether to take inputs for the very first time
+		// or taking input to continue with the previous calculation
 		if flag == 0 {
 			fmt.Print("\nEnter first number: ")
 			fmt.Scan(&number1)
@@ -26,6 +30,7 @@ func main() {
 			fmt.Scan(&number2)
 		}
 
+		// performing calculation
 		switch operationChoice {
 		case 1:
 			result = number1 + number2
@@ -42,6 +47,7 @@ func main() {
 
 		fmt.Println("The result is ", result)
 
+		// controling loop: whether to continue or break out of it
 		fmt.Print("To continue enter 0, otherwise 1 to quit: ")
 		fmt.Scan(&flag)
 		if flag == 1 {
